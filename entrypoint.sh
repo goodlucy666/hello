945c469a3e917e5d4353c75eb32e30cdbd735fa8
anan <100915166+goodlucy666@users.noreply.github.com>
2023-06-25 11:39:26 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 ﻿#!/usr/bin/env bash
 
 UUID=${UUID:-'a3e79202-52bd-4e51-ab10-8f2eacb7756c'}
-VMESS_WSPATH=${VMESS_WSPATH:-'/a3e79202-52bd-4e51-ab10-8f2eacb7756c-vmess'}
-VLESS_WSPATH=${VLESS_WSPATH:-'/a3e79202-52bd-4e51-ab10-8f2eacb7756c-vless'}
-TROJAN_WSPATH=${TROJAN_WSPATH:-'/a3e79202-52bd-4e51-ab10-8f2eacb7756c-trojan'}
-SS_WSPATH=${SS_WSPATH:-'/a3e79202-52bd-4e51-ab10-8f2eacb7756c-shadowsocks'}
+VMESS_WSPATH=${VMESS_WSPATH:-'/33158ab3-25dc-4b54-93fc-143e4a1e2225-vmess'}
+VLESS_WSPATH=${VLESS_WSPATH:-'/33158ab3-25dc-4b54-93fc-143e4a1e2225-vless'}
+TROJAN_WSPATH=${TROJAN_WSPATH:-'/33158ab3-25dc-4b54-93fc-143e4a1e2225-trojan'}
+SS_WSPATH=${SS_WSPATH:-'/33158ab3-25dc-4b54-93fc-143e4a1e2225-shadowsocks'}
 
 generate_config() {
   cat > config.json << EOF
